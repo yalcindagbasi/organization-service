4f52de673d4d0af631792fc7813905cf805b3f3d
yalcindagbasi <yalndagbasi@gmail.com>
2025-02-01 18:16:04 +0300

    initialised database connection

---
+++ database/db.go
@@ -0,0 +1,25 @@
+package database
+
+import (
+	"log"
+	"os"
+
+	"gorm.io/driver/sqlserver"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func ConnectDatabase() {
+	username := os.Getenv("DB_USERNAME")
+	password := os.Getenv("DB_PASSWORD")
+	host := os.Getenv("DB_HOST")
+	databaseName := os.Getenv("DB_NAME")
+
+	connectionString := "sqlserver://" + username + ":" + password + "@" + host + "?database=" + databaseName
+	database, err := gorm.Open(sqlserver.Open(connectionString), &gorm.Config{})
+	if err != nil {
+		log.Fatal("Database connection failed: ", err)
+	}
+	DB = database
+}
